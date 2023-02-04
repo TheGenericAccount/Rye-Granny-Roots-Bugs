add6d6966095e2b1b0011403f17af530b81fad9f
Dovydas <labanauskas.dovydas@gmail.com>
2023-02-04 18:46:30 +0200

    screenshot

--- Plants/Plant.gd
+++ Plants/Plant.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 export (float) var health=100
+export (float) var growthPeriod=30
+const randomness=10
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +10,11 @@ export (float) var health=100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	setGrowthDelay()
 
-
+func setGrowthDelay():
+	$GrowthCycle.wait_time=growthPeriod+rand_range(-randomness, randomness)
+	$GrowthCycle.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
